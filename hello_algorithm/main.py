f3b652f2c18b6469c3b4f89c04b773e5bc234b9c
suiyuano <1095529505@qq.com>
2023-10-25 22:15:59 +0800

    update main.py

--- hello_algorithm/main.py
+++ hello_algorithm/main.py
@@ -4,13 +4,19 @@ Creation date:  2023/10/25
 '''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def the_3_chapter():
+    '''
+    常见的数据结构包括数组、链表、栈、队列、哈希表、树、堆、图，它们可以从“逻辑结构”和“物理结构”两个维度进行分类。
+    :return: 
+    '''
+
+
+def main():
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
